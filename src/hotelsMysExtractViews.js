8983c264f159cc5239525ea38afd3a0ee27e192b
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-06-10 20:42:54 +0200

    Implement hotelsMysExtractViews.flat

--- src/hotelsMysExtractViews.js
+++ src/hotelsMysExtractViews.js
@@ -7,6 +7,20 @@
         })[0].values[0];
     };
 
+    exports.flat = function (hoteldataId, multi) {
+        return [hoteldataId].concat(multi.reduce(function (memo, item) {
+            return memo.concat(item.values);
+        }, []).filter(function (synonym) {
+            return synonym !== hoteldataId;
+        }).reduce(function (memo, synonym) {
+            if (memo.indexOf(synonym) === -1) {
+                return memo.concat([synonym]);
+            }
+
+            return memo;
+        }, []));
+    };
+
     exports.mapHotel = function (emit, doc) {
         emit(doc._id, {
             name: doc.name,
--- tests/hotelsMysExtractViews.unit.test.js
+++ tests/hotelsMysExtractViews.unit.test.js
@@ -3,7 +3,8 @@ describe('hotelsMysExtractViews', function () {
 
     var assert = require('assert'),
         hotelsMysExtractViews = require('../src/hotelsMysExtractViews'),
-        localized = hotelsMysExtractViews.localized;
+        localized = hotelsMysExtractViews.localized,
+        flat = hotelsMysExtractViews.flat;
 
     describe('localized', function () {
         it('basically works', function () {
@@ -17,6 +18,23 @@ describe('hotelsMysExtractViews', function () {
                 }], 'de_CH'),
 
                 'Luzern'
+            );
+        });
+    });
+
+    describe('flat', function () {
+        it('basically works', function () {
+            assert.deepEqual(
+                flat('Luzern', [
+                    {locale: 'en_US', values: ['Lucerne', 'Luzern']},
+                    {locale: 'de_CH', values: ['Luzern']},
+                    {locale: 'es_ES', values: ['Lucerna', 'Luzern']},
+                    {locale: 'fr_FR', values: ['Lucerne', 'Luzern']},
+                    {locale: 'it_IT', values: ['Lucerna', 'Luzern']},
+                    {locale: 'nl_NL', values: ['Luzern']}
+                ]),
+
+                ['Luzern', 'Lucerne', 'Lucerna']
             );
         });
     });
